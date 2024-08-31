d9b88d53429d82935b7e726d196df48ea9b335d5
PriyanshiKevre1 <22103263@MAIL.JIIT.AC.IN>
2024-08-31 10:46:50 +0530

    Add files via upload

---
+++ eval1.py
@@ -0,0 +1,11 @@
+task={
+    "task1":{"id":101,"status":"not done"},
+    "task2":{"id":102,"Status":"done"},
+    "task3":{"id":103,"status":"not done"},
+    "task4":{"id":104,"status":"done"}
+    }
+task["task2"]={"task5":{"id":102,"status":"done"}}
+print(task)
+
+
+
